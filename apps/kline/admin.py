bca167a0a13ba7e000094300e3d1cf50becf8bc7
Qiuqi777 <qiuqi@Klipc.com>
2026-04-27 14:35:33 +0800

    up

--- apps/kline/admin.py
+++ apps/kline/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.kline.models import SymbolHistoryPrice
 
-# Register your models here.
+@admin.register(SymbolHistoryPrice)
+class SymbolHistoryPriceAdmin(admin.ModelAdmin):
+    """用户信息管理（简化版）"""
+
+    list_display = ['symbol', 'open', 'close', 'high', 'low', 'ticket']
